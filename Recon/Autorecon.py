b47d2445f040e2251da5c65ea3fa31bd51e32186
Arpit Goyal <49126445+Silversgit@users.noreply.github.com>
2019-10-09 16:10:22 +0530

    Create Autorecon.py

---
+++ Recon/Autorecon.py
@@ -0,0 +1,67 @@
+from __future__ import absolute_import, print_function
+import sys
+import socket
+import subprocess
+from time import sleep
+
+def main():
+    """Execute main code."""
+    try:
+        domain = sys.argv[1]
+        ip_address = socket.gethostbyname(domain)
+    except IndexError:
+        print('Error: Domain name not specified.')
+        sys.exit(1)
+    except socket.gaierror:
+        print('Error: Domain name cannot be resolved.')
+        raise
+    procs = []
+    whois_cmd = ['whois', domain]
+    dig_cmd = ['dig', '-t', 'txt', '+short', domain]
+    wpscan_cmd = ['wpscan', '--force', '--update', '--url', domain]
+    nmap_hosts_cmd = ['nmap', '-sn', ip_address + '/24']
+    nmap_script_names = ('*-vuln*, banner, default, dns-brute,'
+                         'dns-zone-transfer, ftp-*, hostmap-ip2hosts, http-config-backup,'
+                         'http-cross*, http-devframework, http-enum, http-headers,'
+                         'http-shellshock, http-sitemap-generator, http-waf-fingerprint,'
+                         'http-xssed, smtp-*, ssl-*, version')
+    nmap_full_cmd = ['nmap', '-sV', '-sS', '-A', '-Pn', '--script',
+                     nmap_script_names, domain]
+    cmds = {'TXT Records': dig_cmd, 'WHOIS Info': whois_cmd,
+            'Active Hosts': nmap_hosts_cmd, 'Nmap Results': nmap_full_cmd,
+            'WPScan': wpscan_cmd}
+
+    def handle_proc(proc):
+        """Handle subprocesses outputs."""
+        separator = '=================='
+        output = b''.join(proc.stdout.readlines()).decode('utf-8')
+        print(proc.title)
+        print(separator)
+        print(output.strip())
+        print(separator + '\n')
+        procs.remove(proc)
+
+    for title, cmd in cmds.items():
+        try:
+            proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+            proc.title = title
+            procs.append(proc)
+        except OSError:
+            print('%s >> Dependency error occurred.\n' % title)
+
+    while True:
+        for proc in procs:
+            retcode = proc.poll()
+            if retcode is not None:
+                handle_proc(proc)
+            else:
+                continue
+        if not procs:
+            break
+        else:
+            sleep(1)
+
+if __name__ == '__main__':
+    print('The Process is Running\n')
+    print('Take a Power nap and see Resukts\n')
+    main()
